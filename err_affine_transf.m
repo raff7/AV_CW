60632709b83bc66e48207157e6ee0f21eb01b1b4
Raffa <rf>
2019-03-09 16:04:00 +0000

    yey funzia

--- err_affine_transf.m
+++ err_affine_transf.m
@@ -1,14 +1,14 @@
 function [dist] = err_affine_transf(model, data)
 % Split the data in source points and target points
-source_pts = data(:, 1:3);
-target_pts = data(:, 4:6);
+source_pts = data(:, 1:4);
+target_pts = data(:, 5:8);
 
 % Prepare data for affine transformation
-source_pts = [source_pts; ones(len(data), 1)];
+%source_pts = [source_pts, ones(size(data,1), 1)];
 
 % Transform source data and remove last column (all ones for affine transf)
 transf_source = source_pts * model;
-transf_source = transf_source(:, 1:3);
+%transf_source = transf_source(:, 1:3);
 
 % Calculate the euclidian distance between projected points and targets
 residuals = transf_source-target_pts;
